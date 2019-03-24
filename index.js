f512da6e04579b48291abd92dbb7e570d07c9790
kety-folf <48892500+kety-folf@users.noreply.github.com>
2019-03-25 11:29:36 +1300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,162 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "+";
+const opus = require("node-opus")
+var rate = 64000;
+var encoder = new opus.OpusEncoder(rate);
+const song = ['\song1.mp3', '\song2.mp3', '\song3.mp3', '\song4.mp3', '\song5.mp3'];
+const ffmpeg = require("ffmpeg")
+client.on("ready", () => {
+    console.log("Connected as " + client.user.tag)
+    client.user.setActivity("prefix is +");
+});
+client.on("message", (message) => {
+
+    if (!message.content.startsWith(prefix)) return;
+
+    if (message.content.startsWith(prefix + "ping")) {
+        message.channel.send("pong!");
+        return;
+    }
+
+    if (message.content.startsWith(prefix + "info")) {
+        message.channel.send("bot was made by Kety_the_folf#1470 coded in JS with discord.js");
+        return;
+    }
+
+    if (message.content.startsWith(prefix + "help")) {
+        message.channel.send("bot prefx is +");
+        message.channel.send("comands");
+        message.channel.send("ping: sends the bots ping");
+        message.channel.send("info: info about the bot");
+        message.channel.send("test: sends you a private message")
+        message.channel.send("test2:sends a test message")
+        message.channel.send("play:joins voice chat and starts playing a song see +songs for comannd and name")
+        message.channel.send("songs:lists the songs the bot can play")
+        message.channel.send("stop:stops music")
+        message.channel.send("playYT:plays music from youtube")
+        return;
+    }
+
+    if (message.content.startsWith(prefix + "test")) {
+        message.author.send("this is a test");
+        return;
+    }
+
+    if (message.content.startsWith(prefix + "test2")) {
+        return message.reply("test");
+        return;
+        
+   
+    }
+    if (message.content.startsWith(prefix + "play1")) {
+        client.user.setActivity("music");
+        var VC = message.member.voiceChannel;
+        if (!VC)
+            return message.reply("join a voice chat")
+        VC.join()
+            
+            .then(connection => {
+                message.channel.send("now playing driving into the sun by pepper coyote")
+                const dispatcher = connection.playFile(song[0]);
+                dispatcher.on("end", end => { VC.leave(); client.user.setActivity("prefix is +"); message.channel.send("song finished");  });
+            })
+            .catch(console.error);
+        return;
+    }
+    if (message.content.startsWith(prefix + "play2")) {
+        client.user.setActivity("music");
+        var VC = message.member.voiceChannel;
+        if (!VC)
+            return message.reply("join a voice chat")
+        VC.join()
+
+            .then(connection => {
+                message.channel.send("now playing reg line by foxes and peppers")
+                const dispatcher = connection.playFile(song[1]);
+                dispatcher.on("end", end => { VC.leave(); client.user.setActivity("prefix is +"); message.channel.send("song finished"); });
+            })
+            .catch(console.error);
+        return;
+    }
+    if (message.content.startsWith(prefix + "play3")) {
+        client.user.setActivity("music");
+        var VC = message.member.voiceChannel;
+        if (!VC)
+            return message.reply("join a voice chat")
+        VC.join()
+
+            .then(connection => {
+                message.channel.send("now playing all the single furries")
+                const dispatcher = connection.playFile(song[2]);
+                dispatcher.on("end", end => { VC.leave(); client.user.setActivity("prefix is +"); message.channel.send("song finished"); });
+            })
+            .catch(console.error);
+        return;
+    }
+    if (message.content.startsWith(prefix + "play4")) {
+        client.user.setActivity("music");
+        var VC = message.member.voiceChannel;
+        if (!VC)
+            return message.reply("join a voice chat")
+        VC.join()
+
+            .then(connection => {
+                message.channel.send("now playing knock,knock by Niic")
+                const dispatcher = connection.playFile(song[3]);
+                dispatcher.on("end", end => { VC.leave(); client.user.setActivity("prefix is +"); message.channel.send("song finished"); });
+            })
+            .catch(console.error);
+        return;
+    }
+    if (message.content.startsWith(prefix + "play5")) {
+        client.user.setActivity("music");
+        var VC = message.member.voiceChannel;
+        if (!VC)
+            return message.reply("join a voice chat")
+        VC.join()
+
+            .then(connection => {
+                message.channel.send("now playing star by pepper coyote")
+                const dispatcher = connection.playFile(song[4]);
+                dispatcher.on("end", end => { VC.leave(); client.user.setActivity("prefix is +"); message.channel.send("song finished"); });
+            })
+            .catch(console.error);
+        return;
+    }
+    if (message.content.startsWith(prefix + "playYT")){
+        message.channel.send("work in progess comannd does not work")
+    }
+    if (message.content.startsWith(prefix + "stop")) {
+        client.user.setActivity("bot prefix is +");
+        var VC = message.member.voiceChannel;
+        if (!VC)
+            return message.reply("join a voice chat")
+        VC.join()
+
+            .then(connection => {
+                message.channel.send("stopping music")
+                const dispatcher = connection.playFile();
+                dispatcher.on("end", end => { VC.leave(); client.user.setActivity("prefix is +"); });
+            })
+            .catch(console.error);
+        return;
+    }
+        if (message.content.startsWith(prefix + "songs")) {
+            message.channel.send("songs you can play ")
+            message.channel.send("driving into the sun by pepper coyote = +play1 ")
+            message.channel.send("  reg line by foxes and peppers = +play2 ")
+            message.channel.send("all the single furries = +play3 ")
+            message.channel.send("knock,knock by Niic = +play4")
+            message.channel.send("star by Pepper Coyote = +play5")
+            return;
+        
+    }
+   
+    if (message.content.startsWith(prefix + "quit")) {
+        client.destroy()
+        return
+    }
+   
+});
+client.login("NDI1MTM4NzcyMjIxMzYyMTc2.D2IY6w.dMMBRFiN1XQVBRYmSxzmwHlaEv4");
