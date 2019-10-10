f9613f25535a7ad40b6e7da75ed871a808242143
kety-folf <keatonstorer@gmail.com>
2019-10-10 02:15:31 +0000

    fix 8ball commad

--- commands/command/8ball.js
+++ commands/command/8ball.js
@@ -1,9 +1,9 @@
-module.exports.run = async (folf, message, args, embedErr,embedimg,embedlink,embedtxt) => {
+module.exports.run = async (folf, message, args, embedErr,embedimg,embedlink,embedtxt, arg) => {
     const db = require('quick.db')
     db.add('count.8ball',1)
     let question = arg.substring(6).trim(' ');
     if(!question) return embedErr('error', 'no question given');
-    var responces = [ "It is certain", "It is decidedly so", "Without a doubt",
+    var stuff = [ "It is certain", "It is decidedly so", "Without a doubt",
     "Yes, definitely", "You may rely on it", "As I see it, yes",
     "Most likely", "Outlook good", "Signs point to yes", "Yes",
     "Reply hazy, try again", "Ask again later",
@@ -12,8 +12,8 @@ module.exports.run = async (folf, message, args, embedErr,embedimg,embedlink,emb
     "My reply is no", "My sources say no", "Outlook not so good",
     "Very doubtful"];
 
-    message.channel.send(responces[Math.floor(Math.random()*answers.length)]);
-      
+ 
+      embedtxt('8ball', stuff[Math.floor(Math.random()*stuff.length)])
       };
       
      
