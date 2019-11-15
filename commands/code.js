dfa6b6e9e5aedf7d58a83f6f33179d6648383dc5
Abner <25711694+AbnerSquared@users.noreply.github.com>
2019-11-15 12:55:12 -0600

    Minor organization
    Just a bit of code cleanup, mainly with spacing.

--- commands/code.js
+++ commands/code.js
@@ -1,17 +1,15 @@
-module.exports.run = async (ctx) =>
-{
+module.exports.run = async (ctx) => {
     return ctx.sendEmbed(
-      'code',
-      'if you want to look at my code',
-      'https://github.com/kety-folf/discord-bot-js'
-    );
+      "code",
+      "if you want to look at my code",
+      "https://github.com/kety-folf/discord-bot-js");
 };
-  
+
 module.exports.info = {
-  name: "code",
-  description: "shows github code link",
-  usage: "",
-  category: "misc",
-  accessableby: "members",
- aliases: ["github"]
+    name: "code",
+    description: "shows github code link",
+    usage: "",
+    category: "misc",
+    accessableby: "members",
+    aliases: ["github"]
 };
