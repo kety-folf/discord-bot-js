e9c31e98bae6c0a455955bb608d812880f6211e0
Abner <25711694+AbnerSquared@users.noreply.github.com>
2019-11-15 13:04:12 -0600

    Remove 'coin-flipper' dependency
    Removed an unneeded dependency called 'coin-flipper', which simply randomly gets a number, and returns heads or tails. The code was changed to flip the coin within this file, and everything else was slightly cleaned up.

--- commands/coinFlip.js
+++ commands/coinFlip.js
@@ -1,39 +1,35 @@
-//const coin = require ('coin-flipper');
-module.exports.run = async (ctx) =>
-{
-    var flip = Math.round(Math.random() * 1) == 1 ? 'heads' : 'tails';
-    var bet = 5;
-    var account = ctx.getAccount(ctx.user.id);
-
-    var guess = args[0];
-
+module.exports.run = async (ctx) => {
+    const flip = Math.round(Math.random() * 1) == 1 ? "heads" : "tails";
+    const bet = 5;
+    const guess = args[0];
+    
     if (!guess || guess.toLowerCase() != "heads" || guess.toLowerCase() != "tails")
-      return ctx.error('error', 'heads... or tails? you cant just be typing iajgejuhbjmfrt');
-
+        return ctx.error("error", "heads... or tails? you cant just be typing iajgejuhbjmfrt");
+    
+    var account = ctx.getAccount(ctx.user.id);
+    
     if (account.balance < bet)
-      return ctx.error('error', 'you dont have any money');
+        return ctx.error("error", "you dont have any money");
 
-    var reward =  bet * ctx.utils.getRandNum(24, 2);
-
+    const reward =  bet * ctx.utils.getRandNum(24, 2);
     account.take(bet);
-    var flipTitle = `the coin landed on ${flip}`;
-    var flipResult = "you lost $5";
-
-    if (flip.toLowerCase() == guess)
-    {
+    
+    const flipTitle = `the coin landed on ${flip}`;
+    var flipResult = `you lost \$${bet}`;
+    
+    if (flip.toLowerCase() == guess) {
         account.give(reward);
         flipResult = `you won \$${reward}`;
     }
-
+    
     account.sync();
     return ctx.sendEmbed(flipTitle, flipResult);
 };
-  
+
 module.exports.info = {
-  name: 'coinflip',
-  description: 'flip a coin for money cost $5',
-  usage: '<heads/tails> must be in upper case to work',
-  category: 'money',
-  accessableby: 'members'
-  //aliases: []
+    name: "coinflip",
+    description: "flip a coin for money cost $5",
+    usage: "<heads/tails>",
+    category: "money",
+    accessableby: "members"
 };
