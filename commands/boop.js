5ae8e79c26e2db763c92b9feee1720834dab4c2f
Abner <25711694+AbnerSquared@users.noreply.github.com>
2019-11-15 12:51:31 -0600

    Change boopReply parsing
    Instead of redoing the whole string, new parts can be attached onto the base string, which is 'user has booped '. I also removed the requirements for mentioning, so a user can still boop themselves.

--- commands/boop.js
+++ commands/boop.js
@@ -1,34 +1,27 @@
-module.exports.run = async (ctx) =>
-{
-  const boopImageUrl = "https://cdn.discordapp.com/attachments/597631229357064195/598388881490051202/Cbn90rRUsAAuhCh.jpg";
+module.exports.run = async (ctx) => {
+    // Might want to move this elsewhere, or have a consistent source location.
+    const boopImageUrl = "https://cdn.discordapp.com/attachments/597631229357064195/598388881490051202/Cbn90rRUsAAuhCh.jpg";
+    
+    const user = ctx.user;
+    const user1 = ctx.message.mentions.users.first();
+    const user2 = ctx.message.mentions.users.last();
+    
+    var boopReply = `${user} has booped `;
+  
+    if (!user1 || (user == user1 && user1 == user2))
+        boopReply += "them self";
+    else if (user1 != user2 && user1 != user && user2 != user)
+        boopReply += `${user1} and ${user2}`;
+    else
+        boopReply += `${user1}`;
 
-  var user1 = ctx.message.mentions.users.first();
-  var user2 = ctx.message.mentions.users.last();
-
-  if (!user1)
-    return ctx.error('Error', "@ a user you want to boop");
-
-  var user = ctx.user;
-
-  var boopReply = `${user} has booped them self`;
-
-  if (user1 != user2 && user1 != user && user2 != user)
-  { 
-      boopReply = `${user} has booped ${user1} and ${user2}`;
-  }
-  else
-  {
-      boopReply = ` ${user} has booped ${user1}`;
-  }
-
-  return ctx.sendEmbed('boop', boopReply, null, boopImageUrl);
+    return ctx.sendEmbed("boop", boopReply, "", boopImageUrl);
 };
   
-  module.exports.info = {
-    name: 'boop',
-    description: 'boop a user',
-    usage: '@user or @user @user2',
-    category: 'fun',
-    accessableby: 'members'
-    //aliases: []
-  };
+module.exports.info = {
+    name: "boop",
+    description: "boop a user",
+    usage: "@user or @user @user2",
+    category: "fun",
+    accessableby: "members"
+};
