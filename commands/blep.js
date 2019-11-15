98765b63d90a32c9c25b0408220165419ffdd530
Abner <25711694+AbnerSquared@users.noreply.github.com>
2019-11-15 12:43:54 -0600

    Minor cleanup
    Just some slight extra polish on the code, organizing all of the text values and positions.

--- commands/blep.js
+++ commands/blep.js
@@ -1,17 +1,15 @@
 const FurryBotAPI = require("furrybotapi");
 const fb = new FurryBotAPI("kety-folf's-bot/ pre-1.10.9");
 
-module.exports.run = async (ctx) =>
-{
+module.exports.run = async (ctx) => {
     fb.apiRequest("animals", true, "blep", false)
-      .then(result => ctx.sendEmbed("blep", "", result.imageURL)); 
+        .then(result => ctx.sendEmbed("blep", "", result.imageURL)); 
 };
 
 module.exports.info = {
-  name: 'blep',
-  description: 'I dont know what to put here',
-  usage: '',
-  category: 'fun',
-  accessableby: 'members'
- // aliases: []
+  name: "blep",
+  description: "I dont know what to put here",
+  usage: "",
+  category: "fun",
+  accessableby: "members"
 };
