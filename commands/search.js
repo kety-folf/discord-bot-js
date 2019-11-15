e0e9a267849213fea9098dfbc5f9aea969a13cab
Abner <25711694+AbnerSquared@users.noreply.github.com>
2019-11-15 15:54:41 -0600

    Fix term parsing
    This was using 'args' instead of 'ctx.args' again. This now conjoins the arguments to be one search body as well.

--- commands/search.js
+++ commands/search.js
@@ -1,30 +1,25 @@
 const ytdl = require('ytdl-core');
 const search = require('yt-search');
 
-module.exports.run = async (ctx) =>
-{
-    let term = args[0]; //get text after command
-    search(`${term}`, function (error, result)
+module.exports.run = async (ctx) => {
+    const term = ctx.args.join(" ");
+    search(term, function (error, result)
     {
         if (error)
-          return ctx.error("Error", error);
+            return ctx.error("Error", error);
        
-        let videos = result.videos.slice(1, 11); //get first 10 results
+        const videos = result.videos.slice(1, 11); //get first 10 results
         
-        let response = "";
-
+        var response = "";
         for (var i = 0; i < videos.length; i++)
         {
-            response += `**[${parseInt(i) + 1}]:** \`${videos[i].title}\` - \`https://www.youtube.com${videos[i].url}\``;
+            response += `**[${parseInt(i) + 1}]:** \`${videos[i].title}\` - \`https://www.youtube.com${videos[i].url}\`\n`;
         }
-
         response += `\n**use url with ~play**`;
       
         return ctx.sendEmbed('search', resp);
     });
-  
-  };
-  
+};
  
 module.exports.info = {
   name: 'search',
@@ -32,5 +27,5 @@ module.exports.info = {
   usage: '',
   category: 'music',
   accessableby: 'members',
-  aliases: ['ytSearch']
+  aliases: ['ytsearch']
 };
