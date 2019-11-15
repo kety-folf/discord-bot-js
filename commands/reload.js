f59adba6ea3bae1272c09ef67cf2a5d8c952c177
Abner <25711694+AbnerSquared@users.noreply.github.com>
2019-11-15 16:12:23 -0600

    Fix null references
    Properly set all of the references to now use 'ctx', as required.

--- commands/reload.js
+++ commands/reload.js
@@ -1,28 +1,22 @@
-module.exports.run = async (ctx) =>
-{
-    if(!args[0])
+module.exports.run = async (ctx) => {
+    if(!ctx.args[0])
       return ctx.error('Error', 'Please provide a command to reload!');
 
-    const commandName = args[0].trim().toLowerCase();
+    const commandName = ctx.args[0].trim().toLowerCase();
 
-    if(!folf.commands.get(commandName))
+    if(!ctx.folf.commands.get(commandName))
         return ctx.error('Error', 'That command doesn\'t exist. Try again.');
 
     if (ctx.utils.reloadCommand(commandName))
-    {
-        return ctx.sendEmbed('reload',`Successfully reloaded ${commandName}.js!`);
-    }
+        return ctx.sendEmbed('reload', `Successfully reloaded ${commandName}.js!`);
     else
-    {
-        return ctx.error('Error',`Could not reload: \`${args[0].toUpperCase()}\``);
-    }
+        return ctx.error('Error', `Could not reload: \`${commandName}\``);
 };
- 
+
 module.exports.info = {
   name: 'reload',
   description: 'reloads a command',
   usage: '',
   category: 'misc',
   accessableby: 'dev'
- // aliases: []
 };
