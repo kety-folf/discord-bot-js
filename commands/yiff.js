4493006919c0267e1525ff9be08defb8e1ca7cb8
Abner <25711694+AbnerSquared@users.noreply.github.com>
2019-11-15 13:24:40 -0600

    Inverse NSFW check
    This removes code nesting by checking if the channel was SFW first before performing the request.

--- commands/yiff.js
+++ commands/yiff.js
@@ -1,31 +1,21 @@
 const FurryBotAPI = require("furrybotapi");
 const fb = new FurryBotAPI("kety-folf's-bot/ 1.11.0");
 
-module.exports.run = async (ctx) =>
-{
-    let type = ctx.args[0];
- 
-    if (!type)
-    {
-        type = 'straight';
-    }
+module.exports.run = async (ctx) => {
+    const type = ctx.args[0] || "straight";
 
-    if (ctx.channel.nsfw)
-    {
-        return fb.apiRequest("furry", false, `yiff/${type}`, false)
-                 .then(result => ctx.sendEmbed('yiff', `${type} yiff`, "", result.imageURL));
-    }
-    else
-    {
-	      return ctx.error('Error', 'not nsfw channel');
-    }
+    if (!ctx.channel.nsfw)
+        return ctx.error("Error", "not nsfw channel");
+	
+    return fb.apiRequest("furry", false, `yiff/${type}`, false)
+        .then(result => ctx.sendEmbed("yiff", `${type} yiff`, "", result.imageURL));
 };
 
 module.exports.info = {
-  name: 'yiff',
-  description: 'sends yiff to the channel defalts to straight  ',
-  usage: '[gay | straight]',
-  category: 'NSFW',
-  accessableby: 'members',
-  aliases: []
+    name: "yiff",
+    description: "sends yiff to the channel defalts to straight",
+    usage: "[gay | straight]",
+    category: "NSFW",
+    accessableby: "members",
+    aliases: []
 };
