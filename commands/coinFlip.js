2e1f25b28f9bc7a55ac4bbb94a3e2e368426def6
Abner <25711694+AbnerSquared@users.noreply.github.com>
2019-11-17 18:21:37 -0600

    Apply strict guess check

--- commands/coinFlip.js
+++ commands/coinFlip.js
@@ -8,7 +8,7 @@ module.exports.run = async (ctx) => {
     const guess = ctx.args[0].toLowerCase();
     console.log(guess + ": Coin Guess");
     
-    if ((guess != "heads") || (guess != "tails"))
+    if ((guess !== "heads") || (guess !== "tails"))
         return ctx.error("error", "heads... or tails? you cant just be typing iajgejuhbjmfrt");
     
     var account = ctx.getAccount(ctx.user.id);
