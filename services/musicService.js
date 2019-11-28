c88d348dbbb239f6d7ef1cdc8beb376299e7113e
Abner <25711694+AbnerSquared@users.noreply.github.com>
2019-11-27 21:25:19 -0600

    Change path ref (again)

--- services/musicService.js
+++ services/musicService.js
@@ -1,7 +1,7 @@
 const searchYoutube = require("yt-search");
 const ytdl = require("ytdl-core");
 const YouTube = require("simple-youtube-api");
-const config = require("config.json");
+const config = require("../config.json");
 const youtube = new YouTube(config.youtube_key);
 class Song {
 	constructor(songName, url)
